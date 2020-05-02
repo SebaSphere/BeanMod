5fa49d6f2886223b9f5e8efaa39582e1566eb1e3
BuildTools <unconfigured@null.spigotmc.org>
2020-05-02 18:40:44 -0400

    Initial commit

---
+++ src/main/java/com/terranetichosting/beanmod/util/RegistryHandler.java
@@ -0,0 +1,21 @@
+package com.terranetichosting.beanmod.util;
+
+import com.terranetichosting.beanmod.beanMod;
+import com.terranetichosting.beanmod.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, beanMod.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // Items
+    public static final RegistryObject<Item> BEAN = ITEMS.register("bean", ItemBase::new);
+}
